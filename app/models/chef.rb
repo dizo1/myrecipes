1a91120f47290e91d5c7cf6347396b7c647f93fc
Muganda Dizo <mugandadizo@gmail.com>
2017-05-10 12:57:09 +0000

    Maximumu email letters

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,7 +2,7 @@ class Chef < ApplicationRecord
     before_save { self.email = email.downcase }
     validates :chefname, presence: true, length: {maximum: 15}
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, presence: true, length: { maximum: 20 },
+    validates :email, presence: true, length: { maximum: 30 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
     has_many :recipes
