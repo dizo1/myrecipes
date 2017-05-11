d0de6b26be5a616354736c7092a8eb635b012387
Muganda Dizo <mugandadizo@gmail.com>
2017-05-11 12:08:16 +0000

    Improve layout

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,7 +15,7 @@ class RecipesController < ApplicationController
     
     def create
       @recipe = Recipe.new(recipe_params)
-        @recipe.chef = Chef.first
+        @recipe.chef = current_chef
         if @recipe.save
             flash[:success] = "Recipe created Successfully"
             redirect_to recipe_path(@recipe)
