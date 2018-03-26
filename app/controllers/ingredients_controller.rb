e2f27512d51364cb223bbfffe525b3bc3ad13684
Muganda Dizo <mugandadizo@gmail.com>
2018-03-26 12:12:34 +0000

    Added restrictions to ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,5 +1,6 @@
 class IngredientsController < ApplicationController
     before_action :set_ingred, only: [:edit, :show, :update]
+    before_action :require_admin, except: [:show, :index]
   
   def index
     @ingredients = Ingredient.paginate(page: params[:page], per_page: 3)
@@ -11,11 +12,17 @@ class IngredientsController < ApplicationController
   
   
   def new
-    
+    @ingredient = Ingredient.new
   end
   
   def create
-    
+    @ingredient = Ingredient.new(ingredient_params)
+        if @ingredient.save
+            flash[:success] = "Ingredient Created Successfully"
+            redirect_to ingredient_path(@ingredient)
+        else
+            render 'new'
+        end
   end
   
   def edit
@@ -23,6 +30,12 @@ class IngredientsController < ApplicationController
   end
   
   def update
+      if @ingredient.update(ingredient_params)
+          flash[:success] = "Ingredient updated successfully"
+          redirect_to @ingredient
+      else
+          render 'edit'
+      end
     
   end
   
@@ -30,6 +43,17 @@ end
 
 private
 
+    def require_admin
+        if !logged_in? || (logged_in? and !current_chef.admin?)
+        flash[:danger] = "ONLY ADMIN USERS CAN DO THAT!"
+            redirect_to ingredients_path
+        end
+    end
+
     def set_ingred
         @ingredient = Ingredient.find(params[:id])
+    end
+    
+    def ingredient_params
+        params.require(:ingredient).permit(:name)
     end
